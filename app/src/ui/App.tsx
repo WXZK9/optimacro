d8684e537fc891fb6f9480ba890da634da2f3675
ImBary <bartosz.sm@outlook.com>
2024-12-09 20:09:22 +0100

    czyszczenie

--- app/src/ui/App.tsx
+++ app/src/ui/App.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import snail from './img/snail.png';
 
 import './App.css'
 
@@ -10,22 +10,19 @@ function App() {
     <>
       <div>
    
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+        <a href="https://www.linkedin.com/in/bartek-smolibowski-4b2944299/" target="_blank">
+          <img src={snail} className="snail" alt="snail logo" />
+
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>OptiMacro</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+        
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      
     </>
   )
 }

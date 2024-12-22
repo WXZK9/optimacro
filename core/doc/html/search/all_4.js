ac93f91334816574f1b34b42319bee8f0e48ba28
Paweł <67957305+pawelktk@users.noreply.github.com>
2024-12-22 09:19:18 +0100

    documentation update

--- core/doc/html/search/all_4.js
+++ core/doc/html/search/all_4.js
@@ -1,4 +1,9 @@
 var searchData=
 [
-  ['keysequence_0',['keySequence',['../classEventController.html#a6a3067645b815154c3c0a52aaeada390',1,'EventController']]]
+  ['keysequence_0',['keySequence',['../classEventController.html#a6a3067645b815154c3c0a52aaeada390',1,'EventController']]],
+  ['keysequenceadvanced_1',['keySequenceAdvanced',['../classEventController.html#a270d15ebd903197b2ff22267b34370f0',1,'EventController']]],
+  ['keysequencedown_2',['keySequenceDown',['../classEventController.html#aa543e5777056fece0a2a43acb7b78ae0',1,'EventController']]],
+  ['keysequencedownadvanced_3',['keySequenceDownAdvanced',['../classEventController.html#ab52bb3330c463bae61d94fd64bfc349f',1,'EventController']]],
+  ['keysequenceup_4',['keySequenceUp',['../classEventController.html#a06bb5d44d27386c1cafa359ea9c3d5aa',1,'EventController']]],
+  ['keysequenceupadvanced_5',['keySequenceUpAdvanced',['../classEventController.html#a505beea034f24debeeabb3e9c76db08a',1,'EventController']]]
 ];

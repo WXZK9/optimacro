bfbe8d76595be804d7929f32d36e703ac9197ec4
ImBary <bartosz.sm@outlook.com>
2025-01-21 15:49:07 +0100

    its a start

--- app/src/electron/preload.ts
+++
@@ -1,5 +0,0 @@
-import { contextBridge, ipcRenderer } from 'electron';
-
-contextBridge.exposeInMainWorld('electron', {
-  saveJsonToFile: (data: any) => ipcRenderer.invoke('save-json-to-file', data),
-});
